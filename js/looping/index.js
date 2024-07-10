6523028aee6654bffc4320d5dd1e7af66c05c512
Rahul Patel <rahulpatel43510@gmail.com>
2024-07-10 10:37:41 +0530

    A JavaScript folder has been created here and it has a solution for exercises.

---
+++ js/looping/index.js
@@ -0,0 +1,17 @@
+// normal loop
+let a = 0;
+for (let i = 0; i < 10; i++) {
+  console.log(a + i);
+}
+// any obj form me 
+let obj = {
+  name: "rahul",
+  role: "programmer",
+  complay: "RRR",
+}
+
+for (const key in obj) {
+  // const element = obj[key];
+  console.log(obj)
+
+}
