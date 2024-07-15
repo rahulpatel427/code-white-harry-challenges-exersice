695ba3d7e131a88c644824d21d8f3d7ca48a7e45
Rahul Patel <rahulpatel43510@gmail.com>
2024-07-15 12:52:46 +0530

    remove node_module

--- Express js/main2.js
+++ Express js/main2.js
@@ -3,6 +3,11 @@ const app = express()
 const port = 3000
 
 
+app.use(express.static("public")); // yha public ke bjay kuch or bhi ho skta he.. public just a name but this is a good standerd 
+// ab mujhe kahi files ko apload krna he to kya me. pura backend source code thoda na dunga me  
+// me sirf public files hi dunga or yhi pr kamal aata he express js kyuki iska use krke hm ek files ko de skte jo kam ki he vo baki code hmare pass hi rhega..
+
+
 // app.get , app.put, app.delete and app.post(path, handler)
 app.get('/', (req, res) => {
   res.send('jay ma renuka!')
@@ -11,6 +16,7 @@ app.get('/', (req, res) => {
 // ye ek yha hmne slug name ka variable create kr liya he. juska mtlb he ki browser ke url me jo bhi likhe vo print hoga
 app.get('/home/:slug', (req, res) => {
   res.send(`jay ma ${req.params.slug}!`)
+  console.log(req.params);
 })
 
 
---
+++ Express js/public/readme.txt
@@ -0,0 +1,7 @@
+
+ Note: jb bhi code ko run krna ho to node_module ko install krna na bhule (npm i)
+1. Why should we use express ?
+  2. why nodemon ?
+    3. installing Express @4
+4. Request parameters and MediaQueryListEvents
+5. Static Files
