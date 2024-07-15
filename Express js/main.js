a47dbca0f7d49caace65caf3f079aeee82787505
Rahul Patel <rahulpatel43510@gmail.com>
2024-07-15 12:30:11 +0530

    Adding a new file Express js and create a js files to usful code.

--- Express js/main.js
+++ Express js/main.js
@@ -4,9 +4,9 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plai')
-  res.end('Hello World\n')
+  res.end('Hello World2\n')
 
 })
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`)
-})
+}) 
