26d79e2d2090b3f8d7d44af6fb6a32ccda8305bc
Rahul Patel <rahulpatel43510@gmail.com>
2024-07-26 15:11:38 +0530

    useCallback-Hook function ko memories krta he

---
+++ ReactJs/useCallback-hook/src/App.jsx
@@ -0,0 +1,42 @@
+import { useState, useCallback } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Navbar from "./componets/Navbar";
+
+function App() {
+  const [count, setCount] = useState(0);
+  const [adjective, setAdjective] = useState("good");
+
+  const getAdjective = useCallback(() => {
+    return "another" + count;
+  }, [count]);
+
+  return (
+    <>
+      <Navbar adjective={adjective} count={count} getAdjective={getAdjective} />
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  );
+}
+
+export default App;
